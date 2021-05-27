b67e553813a176aa8c2465bb235aa70bad513bc4
Cyd Villavicencio <Villacv@Cyds-MBP.fios-router.home>
2021-05-27 16:17:28 -0400

    route and seed changes

--- models/product.js
+++ models/product.js
@@ -3,10 +3,12 @@ const Schema = mongoose.Schema
 
 const Product = new Schema(
   {
-    name: { type: String, required: true },
-    imgURL: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: String, required: true }
+    name:  {type: String, required: true}, 
+    price: {type: String, required: true},
+    body: {type: String, required: true},
+    description: {type: String, required: true},
+    username: {type: String, required: true},
+    imgURL: {type: String, required: true},
   },
   { timestamps: true }
 )
