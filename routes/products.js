5006d896c4c6d0c99b58fa92073ed079bdea84c8
Anss Chaudhry <ansschaudhry@ansss-mac-mini.lan>
2021-05-28 12:28:18 -0400

    finished debugging

--- routes/products.js
+++ routes/products.js
@@ -4,10 +4,10 @@ import restrict from "../helpers/restrict.js";
 
 const router = Router();
 
-router.get("/products", controllers.getProducts);
-router.get("/products/:id", controllers.getProduct);
-router.post("/products", restrict, controllers.createProduct);
-router.put("/products/:id", restrict, controllers.updateProduct);
-router.delete("/products/:id", restrict, controllers.deleteProduct);
+router.get('/products', controllers.getProducts);
+router.get('/products/:id', controllers.getProduct);
+router.post('/products', restrict, controllers.createProduct);
+router.put('/products/:id', restrict, controllers.updateProduct);
+router.delete('/products/:id', restrict, controllers.deleteProduct);
 
 export default router;
