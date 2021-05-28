de1ee53e97871b06d9e21190561f1dc18705b0f0
Anss Chaudhry <ansschaudhry@ansss-mac-mini.lan>
2021-05-28 12:28:18 -0400

    finished debugging

--- seed/data.js
+++ seed/data.js
@@ -32,7 +32,7 @@ const insertData = async () => {
       name: "Product 001",
       imgURL:
         "//img.ltwebstatic.com/images3_pi/2020/07/31/1596179283bf80635c37cba274cb5eb3a726751565_thumbnail_600x.webp",
-      description: "",
+      description: "Placeholder",
       price: "125",
       size: " ",
       username: "admin",
@@ -41,7 +41,7 @@ const insertData = async () => {
       name: "Product 002",
       imgURL:
         "//img.ltwebstatic.com/images3_pi/2020/04/14/1586833567c084a75c03ce8e463774c1e4de8b0e55_thumbnail_600x.webp",
-      description: "",
+      description: "Placeholder",
       price: "140",
       size: " ",
       username: "admin",
@@ -50,7 +50,7 @@ const insertData = async () => {
       name: "Product 003",
       imgURL:
         "//img.ltwebstatic.com/images3_pi/2020/10/29/160395212189e53bc4c916996ecb0449bc514fed5d_thumbnail_600x.webp",
-      description: "",
+      description: "Placeholder",
       price: "140",
       size: " ",
       username: "admin",
@@ -59,7 +59,7 @@ const insertData = async () => {
       name: "Product 004",
       imgURL:
         "//img.ltwebstatic.com/images3_pi/2020/08/24/15982470023b5aa5b0439f567707eaf32d86974f1f_thumbnail_600x.webp",
-      description: "",
+      description: "Placeholder",
       price: "168",
       size: " ",
       username: "admin",
@@ -68,7 +68,7 @@ const insertData = async () => {
       name: "Product 005",
       imgURL:
         "//img.ltwebstatic.com/images3_pi/2020/05/26/159048436604cb5e6693276211be18ce543eed704e_thumbnail_600x.webp",
-      description: "",
+      description: "Placeholder",
       price: "110",
       size: " ",
       username: "admin",
@@ -77,7 +77,7 @@ const insertData = async () => {
       name: "Product 006",
       imgURL:
         "//img.ltwebstatic.com/images3_pi/2020/12/12/1607747413e77b308a7a1d3a2043d4d87a4e8712b0_thumbnail_600x.webp",
-      description: "",
+      description: "Placeholder",
       price: "130",
       size: " ",
       username: "admin",
@@ -86,7 +86,7 @@ const insertData = async () => {
       name: "Product 007",
       imgURL:
         "//img.ltwebstatic.com/images3_pi/2020/12/12/1607747413e77b308a7a1d3a2043d4d87a4e8712b0_thumbnail_600x.webpg",
-      description: "",
+      description: "Placeholder",
       price: "130",
       size: " ",
       username: "admin",
@@ -95,7 +95,7 @@ const insertData = async () => {
       name: "Product 008",
       imgURL:
         "https://ih1.redbubble.net/image.418002069.6282/ssrco,classic_tee,womens,fafafa:ca443f4786,front_alt,square_product,600x600.u2.jpg",
-      description: "",
+      description: "Placeholder",
       price: "130",
       size: " ",
       username: "admin",
