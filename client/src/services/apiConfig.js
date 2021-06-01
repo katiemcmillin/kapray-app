db0a30d84788c7f4a264de1412df73f94f74a06f
Anss Chaudhry <ansschaudhry@ansss-mac-mini.lan>
2021-06-01 11:38:38 -0400

    added correct api link in apiConfig

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -9,7 +9,7 @@ const getToken = () => {
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://sei-products-api.herokuapp.com/api"
+      ? "https://kapray-sun.herokuapp.com/api"
       : "http://localhost:3000/api",
 });
 
