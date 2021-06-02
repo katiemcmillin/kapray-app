97494ae8206c33cfc46443da21687e59ec215eae
Anss Chaudhry <ansschaudhry@ansss-mac-mini.lan>
2021-06-02 02:09:36 -0400

    started home page and nav css

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,11 +1,24 @@
 import "./Home.css";
 import { Layout, ProductCards } from "../../components";
+import { Link } from "react-router-dom";
 
 const Home = (props) => {
   return (
     <Layout user={props.user}>
-      <div className="home">
-        <ProductCards />
+      <div className="home-sign-up">
+        <Link>
+          <h1>SIGN - UP</h1>
+        </Link>
+      </div>
+      <div className="home-apparel">
+        <Link>
+        <h1>APPAREL</h1>
+        </Link>
+      </div>
+      <div className="home-add-apparel">
+        <Link>
+        <h1>ADD APPAREL</h1>
+        </Link>
       </div>
     </Layout>
   );
