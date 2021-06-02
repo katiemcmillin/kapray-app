4df19cce7a8c71f9b74d728b29306d4d374a052c
Katie McMillin <katiemcmillin@Katies-MBP.home>
2021-06-02 10:20:46 -0400

    added nav bar to sign in/sign up

--- client/src/screens/SignOut/SignOut.jsx
+++ client/src/screens/SignOut/SignOut.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { signOut } from '../../services/users'
 import { useHistory } from 'react-router-dom'
 
+
 const SignOut = (props) => {
   const { setUser } = props
   const history = useHistory()
