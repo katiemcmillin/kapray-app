2c69e908eecd8655b94d1e53b795d17e0b039ffb
Cyd Villavicencio <Villacv@Cyds-MBP.fios-router.home>
2021-06-04 10:13:44 -0400

    deleted unused code

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,5 @@
-import Layout from './Layout/Layout'
-import Product from './Product/Product'
-import Search from './Search/Search'
-import Sort from './Sort/Sort'
-import Cart from './Cart/Cart'
-export { Layout, Product, Search, Sort, Cart }
+import Layout from "./Layout/Layout";
+import Product from "./Product/Product";
+import Search from "./Search/Search";
+import Sort from "./Sort/Sort";
+export { Layout, Product, Search, Sort };
