746a9c2317f823ee6c53691950eb1abbf2d2ff9c
katiemcmillin <katiemcmillin@gmail.com>
2023-01-29 20:33:04 -0500

    edited back-end link

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -9,8 +9,8 @@ const getToken = () => {
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://kapray-sun.herokuapp.com/api"
-      : "http://localhost:3000/api",
+      ? "https://kapray-app-backend.onrender.com/api"
+      : "http://localhost:4000/api",
 });
 
 api.interceptors.request.use(
